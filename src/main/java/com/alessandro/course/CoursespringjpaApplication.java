739d309b17ffde62a7754b6a48fd253550e73e66
AlessandroOS <alessandro0014@hotmail.com>
2021-07-14 21:37:27 -0300

    projeto criado + User entity and resourse

---
+++ src/main/java/com/alessandro/course/CoursespringjpaApplication.java
@@ -0,0 +1,13 @@
+package com.alessandro.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursespringjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursespringjpaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/alessandro/course/resources/UserResurce.java
@@ -0,0 +1,19 @@
+package com.alessandro.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alessandro.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResurce {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L,  "Maria", "maria@gmail.com", "99999999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
