e3dc44b0572bcdc10a6da9ee75e5507b4a1b1de3
AlessandroOS <alessandro0014@hotmail.com>
2021-07-15 21:29:13 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/alessandro/course/config/TestConfig.java
+++ src/main/java/com/alessandro/course/config/TestConfig.java
@@ -1,13 +1,15 @@
 package com.alessandro.course.config;
 
-import java.util.Arrays;
+import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.alessandro.course.entities.Order;
 import com.alessandro.course.entities.User;
+import com.alessandro.course.repositories.OrderRepository;
 import com.alessandro.course.repositories.UserRepository;
 
 @Configuration
@@ -16,6 +18,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -23,8 +28,15 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		
 		userRepository.save(u1);
 		userRepository.save(u2);
+		orderRepository.save(o1);
+		orderRepository.save(o2);
+		orderRepository.save(o3);
 	}
 
 }
