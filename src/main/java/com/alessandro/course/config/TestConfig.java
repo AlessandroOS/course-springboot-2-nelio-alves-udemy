64201907dbbe56f6a285ceca76e40f8bbd63974a
AlessandroOS <alessandro0014@hotmail.com>
2021-07-17 21:01:59 -0300

    Payment, one-to-one association

--- src/main/java/com/alessandro/course/config/TestConfig.java
+++ src/main/java/com/alessandro/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 import com.alessandro.course.entities.Category;
 import com.alessandro.course.entities.Order;
 import com.alessandro.course.entities.OrderItem;
+import com.alessandro.course.entities.Payment;
 import com.alessandro.course.entities.Product;
 import com.alessandro.course.entities.User;
 import com.alessandro.course.entities.enums.OrderStatus;
@@ -98,8 +99,10 @@ public class TestConfig implements CommandLineRunner{
 		orderItemRepository.save(oi3);
 		orderItemRepository.save(oi4);
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
 		
-		
+		orderRepository.save(o1);
 
 	}
 
